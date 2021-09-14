e34736bdd02593c18eb698958c6ebede5d5b61ac
VEGA MEDINA <carlos.vega@capgemini.com>
2021-09-14 15:55:30 -0500

    Cambios guardados

--- src/main/java/com/tis/mx/ApplicationStarter.java
+++ src/main/java/com/tis/mx/ApplicationStarter.java
@@ -1,5 +1,17 @@
 package com.tis.mx;
 
-public class ApplicationStarter {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author cavega
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ApplicationStarter {
+	public static void main(String args[]) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
 }
